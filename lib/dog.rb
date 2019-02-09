acfdb5a9ff2d42493ceb5ecfc106fa2f1d72cce4
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-09 02:51:36 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+   
+def name 
+ @name 
+ end 
+ 
+ def name=(new_name)
+  @name= new_name
+ end 
+ 
+ def breed 
+   @breed 
+ end 
+ 
+def breed=(new_breed)
+   @breed= new_breed
+ end 
+end 
+
+
+ 
+
+
+# # Fido.instance_variable_set(:@name, "Fido")
+# # Fido.instance_variable_get(:@name)
+
+
